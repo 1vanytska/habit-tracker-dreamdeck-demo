a304974d1a87f55ae951fdd721c517f71539f152
1vanytska <sofiia.ivanytska.22@pnu.edu.ua>
2024-10-20 22:47:41 +0300

    The first entity - Category

---
+++ src/main/java/com/sofiiaivanytska/goaltrackerdemo/entity/Category.java
@@ -0,0 +1,39 @@
+package com.sofiiaivanytska.goaltrackerdemo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int categoryId;
+
+    private String name;
+
+    public Category() {}
+
+    public Category(int categoryId, String name) {
+        this.categoryId = categoryId;
+        this.name = name;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
