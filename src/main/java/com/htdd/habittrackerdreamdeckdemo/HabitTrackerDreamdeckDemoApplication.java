a5f1415d548d6ffb1f2b56452e83257086824fc2
unknown <sofiia.ivanytska.22@pnu.edu.ua>
2025-04-01 22:28:26 +0300

    Gradle + migrations

---
+++ src/main/java/com/htdd/habittrackerdreamdeckdemo/HabitTrackerDreamdeckDemoApplication.java
@@ -0,0 +1,13 @@
+package com.htdd.habittrackerdreamdeckdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitTrackerDreamdeckDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HabitTrackerDreamdeckDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/htdd/habittrackerdreamdeckdemo/HabitTrackerDreamdeckDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.htdd.habittrackerdreamdeckdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitTrackerDreamdeckDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
