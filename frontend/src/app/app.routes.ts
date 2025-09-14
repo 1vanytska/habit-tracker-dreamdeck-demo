45089000fa7b8da8ada5db4d170b086bbad460da
1vanytska <ivanuksofia7@gmail.com>
2025-09-14 16:01:42 +0300

    Add Home design

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Goal } from './goal/goal';
+import { EditGoal } from './edit-goal/edit-goal';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: Home},
+    { path: 'goal/:id', component: Goal},
+    { path: 'edit-goal/:id', component: EditGoal}
+];
