c8ba47b9394bce8507a24067225206f6cef53092
1vanytska <ivanuksofia7@gmail.com>
2026-02-12 19:39:09 +0200

    feat: setup ecr repo

---
+++ terraform/main.tf
@@ -0,0 +1,57 @@
+provider "aws" {}
+
+terraform {
+    backend "s3" {
+        bucket = "dreamdeck-s3-bucket"
+        key = "ecr.tfstate"
+        region = "eu-west-1"
+    }
+}
+
+resource "aws_ecr_repository" "ecr" {
+    name = var.repo_name
+    image_tag_mutability = "MUTABLE"
+    image_scanning_configuration {
+        scan_on_push = true
+    }
+}
+
+resource "aws_ecr_repository_policy" "public_policy" {
+    repository = aws_ecr_repository.ecr.name
+    policy = jsonencode({
+        version = "2012-10-17"
+        statement = [
+            {
+                sid = "AllowPull"
+                effect = "Allow"
+                principal = "*"
+                action = [
+                    "ecr:GetDownloadUrlForLayer",
+                    "ecr:BatchGetImage",
+                    "ecr:BatchCheckLayerAvailability",
+                    "ecr:GetRepositoryPolicy"
+                ]
+            }
+        ]
+    })
+}
+
+resource "aws_ecr_lifecycle_policy" "lifecycle" {
+  repository = aws_ecr_repository.ecr.name
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description = "Keep last ${var.image_limit} images"
+        selection = {
+          tagStatus = "any"
+          countType = "imageCountMoreThan"
+          countNumber = var.image_limit
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "repo_name" {
+  type = string
+  default = "dreamdeck-ecr"
+}
+
+variable "image_limit" {
+  type = number
+  default = 14
+}
